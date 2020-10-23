6b28a0661e18e5444de70ab579232c1d75b50135
ABHISHEK KUMAR <abhishekkumar@ABHISHEKs-MacBook-Pro.local>
2020-10-23 23:08:00 +0530

    testing cpp mac

---
+++ a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+
+    for (const string& word : msg)s
+    {
+        cout << word << " ";
+    }
+    cout << endl;
+}
